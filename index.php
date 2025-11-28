b62f1ad7caa990b2acdd9591b8d151fbc29919b2
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-11-28 12:43:26 +0100

    setup database connection in index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php 
-    echo "setuping php";
-?>
+    $server_name = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "task_manager";
+
+    $sql = mysqli_connect($server_name,$username,$password,$db_name);
+
+
+    if(!$sql){
+        echo "problem in connexion ";
+    }
+    else{
+        echo "all good ";
+    }
+?>  
