c79dd779782ca44251bbbba812231178849234af
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-11-28 14:00:40 +0100

    refactor task insertion logic in index.php to use form data

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 
     $sql = mysqli_connect($server_name,$username,$password,$db_name);
 
-    $add_task = "INSERT INTO task(task_title,task_description) VALUES('test','testing inserting new item to task')";
     if(!$sql){
         echo "problem in connexion ";
     }
@@ -15,10 +14,42 @@
     }
 
 
-    if(mysqli_query($sql,$add_task)){
-        echo "added";
+    // task informations 
+
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $task_title = $_POST["task_title"];
+    $task_description = $_POST["task_description"];
+
+
+    $task_add = "INSERT INTO task (task_title,task_description) VALUES('$task_title','$task_description')";
+
+    if(mysqli_query($sql,$task_add)){
+        echo "sended";
     }
     else{
         echo "problem";
     }
+    }
+
 ?>  
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task Manager</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <label for="">Title</label>
+        <input type="text" name="task_title">
+        <label for="">Description</label>
+        <textarea name="task_description" id=""></textarea>
+
+        <button type="submit">Add Task</button>
+    </form>
+</body>
+</html>
