44068dc141f31ccd19e1ba660ce462daeab39b82
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-11-28 12:50:10 +0100

    add task insertion logic to index.php

--- index.php
+++ index.php
@@ -6,11 +6,19 @@
 
     $sql = mysqli_connect($server_name,$username,$password,$db_name);
 
-
+    $add_task = "INSERT INTO task(task_title,task_description) VALUES('test','testing inserting new item to task')";
     if(!$sql){
         echo "problem in connexion ";
     }
     else{
         echo "all good ";
+    }
+
+
+    if(mysqli_query($sql,$add_task)){
+        echo "added";
+    }
+    else{
+        echo "problem";
     }
 ?>  
