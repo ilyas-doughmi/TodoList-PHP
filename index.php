43d1446f9b928f75e818b095565409ff55506455
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-11-30 13:49:37 +0100

    fix: correct table name in task insertion query and improve HTML structure

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $password = "";
 $db_name = "task_manager";
 
 $conn = mysqli_connect($server_name, $username, $password, $db_name);
-
+    
 if (!$conn) {
     die("Connection failed");
 }
@@ -15,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $task_title = $_POST["task_title"];
     $task_description = $_POST["task_description"];
 
-    $task_add = "INSERT INTO tasks (task_title, task_description)
+    $task_add = "INSERT INTO task (task_title, task_description)
                  VALUES ('$task_title', '$task_description')";
 
     if (mysqli_query($conn, $task_add)) {
@@ -27,6 +27,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 }
 ?>
 
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -34,18 +37,46 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task Manager</title>
+      <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 
-<body>
-    <form action="index.php" method="POST">
-        <label>Title</label>
-        <input type="text" name="task_title" required>
+<body class="">
+    <div class="flex justify-center flex-col align-center items-center">
+ <h1 class="text-center text-4xl font-semibold ">Task Manager</h1>
+    <form action="index.php" method="POST" class="flex flex-col">
+        
+        <input type="text" name="task_title" required placeholder="Task Title" class="border-2 rounded-xl text-center mb-4 mt-5 h-[40px]">
 
-        <label>Description</label>
-        <textarea name="task_description" required></textarea>
+        <textarea name="task_description" required class="border-2 rounded-[12px] h-[80px] mb-2 text-center" placeholder="Task Description"></textarea>
 
-        <button type="submit">Add Task</button>
+        <button type="submit" class="border-2 rounded-[12px] h-[6vh]  w-[80%] self-center bg-black text-white text hover:scale[0.9px]">Add Task</button>
+        
+        <div class="border-2 border-black mt-12">
+     <?php
+
+        $get_data = "SELECT * FROM task";
+
+        $result = mysqli_query($conn, $get_data);
+
+
+
+
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<h3>" . $row["task_title"] . " | " . $row["task_description"] . "</h3>";
+            }
+        }
+        else{
+            echo "no task found";
+        }
+
+        ?>
+        </div>
+   
     </form>
+    </div>
+   
 </body>
 
 </html>
